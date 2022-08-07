ac78b233ea7ebe8603182f88fb1aaebdbb367e01
vigneshshiv <vichuviky@gmail.com>
2022-08-08 00:13:55 +0530

    Bitwise Operators series

---
+++ code/ctci/operators/PairwiseSwaps.java
@@ -0,0 +1,16 @@
+package code.ctci.operators;
+
+import code.ctci.utils.MethodsUtility;
+
+public class PairwiseSwaps {
+
+    private static int swapOddEvenBits(int x) {
+        return ( ((x & 0xaaaaaaaa) >>> 1) | ((x & 0x55555555) << 1) ) ;
+    }
+
+    public static void main(String[] args) {
+        int swap = swapOddEvenBits(11);
+        System.out.println(MethodsUtility.convertToBinaryString(swap, 4));
+    }
+
+}
