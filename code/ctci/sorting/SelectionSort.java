5272ad69b239aa665ea42a1c592b63f61f95a116
vigneshshiv <vichuviky@gmail.com>
2022-08-17 13:26:34 +0530

    Sorting series

--- code/ctci/sorting/SelectionSort.java
+++ code/ctci/sorting/SelectionSort.java
@@ -1,54 +1,55 @@
 package code.ctci.sorting;
 
 import java.util.Arrays;
-import java.util.function.BiConsumer;
 
-public class BubbleSort implements Sort {
+public class SelectionSort implements Sort {
 
     /**
-     * Time complexity: O(n^2) average and worst case, Best case is O(n)
+     * Time complexity: O(n^2)
      * Space complexity: O(1)
      */
-    private static void bubbleSort(int[] arr) {
-        boolean swapped = false;
-        for (int i = 0; i < arr.length; i++) {
-            swapped = false;
-            for (int j = 1; j < arr.length - i; j++) {
-                if (arr[j] < arr[j - 1]) {
-                    Sort.swap(arr, j, j - 1);
-                    swapped = true;
-                }
-            }
-            if (!swapped) {
-                break;
+    private static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            var first = i;
+            var minIndex = findMinIndex(arr, first);
+            Sort.swap(arr, first, minIndex);
+        }
+    }
+    
+    private static int findMinIndex(int[] arr, int first) {
+        int minIndex = first;
+        for (int j = first + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
             }
         }
+        return minIndex;
     }
 
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
         int[] input = arr.clone();
-        bubbleSort(arr);
+        selectionSort(arr);
         Sort.logger.accept(Arrays.toString(input), Arrays.toString(arr));
         //
         arr = new int[] {1, 2, 3, 4, 5};
         input = arr.clone();
-        bubbleSort(arr);
+        selectionSort(arr);
         Sort.logger.accept(Arrays.toString(input), Arrays.toString(arr));
         //
         arr = new int[] {1};
         input = arr.clone();
-        bubbleSort(arr);
+        selectionSort(arr);
         Sort.logger.accept(Arrays.toString(input), Arrays.toString(arr));
         //
         arr = new int[] {5, 7, -2, 0, -3};
         input = arr.clone();
-        bubbleSort(arr);
+        selectionSort(arr);
         Sort.logger.accept(Arrays.toString(input), Arrays.toString(arr));
         //
         arr = new int[] {};
         input = arr.clone();
-        bubbleSort(arr);
+        selectionSort(arr);
         Sort.logger.accept(Arrays.toString(input), Arrays.toString(arr));
     }
 
