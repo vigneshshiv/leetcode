64c4ea3d92198d99498879698a8febf0488599ae
vigneshshiv <vichuviky@gmail.com>
2022-08-18 00:57:01 +0530

    Sorting series

--- code/java/numbers/DuplicateNumber.java
+++ code/java/numbers/DuplicateNumber.java
@@ -2,11 +2,16 @@ package code.java.numbers;
 
 import code.ctci.sorting.Sort;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiConsumer;
 
 /**
  * https://leetcode.com/problems/find-the-duplicate-number/
+ *
+ * https://leetcode.com/problems/find-all-duplicates-in-an-array/
+ *
  */
 public class DuplicateNumber {
 
@@ -53,6 +58,29 @@ public class DuplicateNumber {
         return slow;
     }
 
+    /**
+     * Time complexity: O(n), where n is the number of elements in the array
+     * Space complexity: O(m)
+     */
+    private static List<Integer> findAllDuplicates(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int index = arr[i] - 1;
+            if (arr[i] != arr[index]) {
+                Sort.swap(arr, index, i);
+            } else {
+                i++;
+            }
+        }
+        List<Integer> numbers = new ArrayList<>();
+        for (int idx = 0; idx < arr.length; idx++) {
+            if (arr[idx] != idx + 1) {
+                numbers.add(arr[idx]);
+            }
+        }
+        return numbers;
+    }
+
     public static void main(String[] args) {
         BiConsumer<String, String> logger = (input, result) -> System.out.println("Input - " + input + ", Result - " + result);
         //
@@ -85,6 +113,21 @@ public class DuplicateNumber {
         input = arr.clone();
         num = findDuplicateOptimal(arr);
         logger.accept(Arrays.toString(input), String.valueOf(num));
+        //
+        arr = new int[] {4, 3, 2, 7, 8, 2, 3, 1};
+        input = arr.clone();
+        List<Integer> numbers = findAllDuplicates(arr);
+        logger.accept(Arrays.toString(input), numbers.toString());
+        //
+        arr = new int[] {1, 1, 2};
+        input = arr.clone();
+        numbers = findAllDuplicates(arr);
+        logger.accept(Arrays.toString(input), numbers.toString());
+        //
+        arr = new int[] {};
+        input = arr.clone();
+        numbers = findAllDuplicates(arr);
+        logger.accept(Arrays.toString(input), numbers.toString());
     }
 
 }
