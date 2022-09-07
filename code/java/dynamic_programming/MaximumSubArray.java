dceb51a6224aa1f611597009e2213a5a0487a9da
vigneshshiv <vichuviky@gmail.com>
2022-09-08 00:02:20 +0530

    Maximum SubArray length, indices and elements

--- code/java/dynamic_programming/MaximumSubArray.java
+++ code/java/dynamic_programming/MaximumSubArray.java
@@ -5,6 +5,8 @@ import java.util.function.BiConsumer;
 
 /**
  * https://leetcode.com/problems/maximum-subarray/
+ *
+ * https://leetcode.com/problems/maximum-product-subarray
  */
 public class MaximumSubArray {
 
@@ -15,7 +17,7 @@ public class MaximumSubArray {
      * Time complexity: O(n^2)
      * Space complexity: O(n)
      */
-    private static int maxSubArray(int[] nums) {
+    private static int maxSubArraySum(int[] nums) {
         int sum = Integer.MIN_VALUE;
         for (int idx = 0; idx < nums.length; idx++) {
             sum = Math.max(sum, subArraySum(idx, 0, nums));
@@ -38,7 +40,7 @@ public class MaximumSubArray {
      * Time complexity: O(n^2)
      * Space complexity: O(n)
      */
-    private static int maxSubArrayOptimal(int[] nums) {
+    private static int maxSubArraySumOptimal(int[] nums) {
         int curr = nums[0], global = nums[0];
         for (int i = 1; i < nums.length; i++) {
             curr = Math.max(curr + nums[i], nums[i]);
@@ -50,28 +52,119 @@ public class MaximumSubArray {
     }
 
     /**
-     * TODO
-     * 1. Length of the max subarray
-     * 2. Elements of the max subarray
-     * 3. Start and End index of max subarray
+     * Length of the max subarray
      */
+    private static int maxSubArraySumLength(int[] nums) {
+        int curr = nums[0], global = nums[0], start = 0, end = 0;
+        for (int i = 1; i < nums.length; i++) {
+            curr += nums[i];
+            if (curr > global) {
+                global = curr;
+                end = i;
+            }
+            if (curr < 0) {
+                curr = 0;
+                start = i + 1;
+            }
+        }
+        return end - start + 1;
+    }
 
+    /**
+     * Start and End index of max subarray
+     */
+    private static int[] maxSubArraySumIndices(int[] nums) {
+        int curr = nums[0], global = nums[0], start = 0, end = 0;
+        for (int i = 1; i < nums.length; i++) {
+            curr += nums[i];
+            if (curr > global) {
+                global = curr;
+                end = i;
+            }
+            if (curr < 0) {
+                curr = 0;
+                start = i + 1;
+            }
+        }
+        return new int[] {start, end};
+    }
 
+    /**
+     * Elements of the max subarray
+     */
+    private static int[] maxSubArraySumElements(int[] nums) {
+        int curr = nums[0], global = nums[0], start = 0, end = 0;
+        for (int i = 1; i < nums.length; i++) {
+            curr += nums[i];
+            if (curr > global) {
+                global = curr;
+                end = i;
+            }
+            if (curr < 0) {
+                curr = 0;
+                start = i + 1;
+            }
+        }
+        return Arrays.copyOfRange(nums, start, end + 1);
+    }
+
+    /**
+     * TODO:
+     * Maximum Product of Sub Array
+     */
+    private static int maxSubArrayProductOptimal(int[] nums) {
+        int curr = nums[0], global = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            curr = Math.max(curr, curr * nums[i]);
+            if (curr > global) {
+                global = curr;
+            }
+        }
+        return global;
+    }
 
     public static void main(String[] args) {
         BiConsumer<int[], Integer> logger = (input, maxSum) -> System.out.println("Input - " + Arrays.toString(input) + ", MaxSum - " + maxSum);
+        BiConsumer<int[], Integer> length_logger = (input, length) -> {
+            System.out.println("Input - " + Arrays.toString(input) + ", Length - " + length);
+        };
+        BiConsumer<int[], int[]> indices_logger = (input, indices) -> {
+            System.out.println("Input - " + Arrays.toString(input) + ", Indices - " + Arrays.toString(indices));
+        };
+        BiConsumer<int[], int[]> elements_logger = (input, elements) -> {
+            System.out.println("Input - " + Arrays.toString(input) + ", Elements - " + Arrays.toString(elements));
+        };
+        BiConsumer<int[], Integer> product_logger = (input, maxProduct) -> {
+            System.out.println("Input - " + Arrays.toString(input) + ", MaxProduct - " + maxProduct);
+        };
         //
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        int maxSum = maxSubArrayOptimal(nums);
+        int maxSum = maxSubArraySumOptimal(nums);
         logger.accept(nums, maxSum);
         //
         nums = new int[] {-1};
-        maxSum = maxSubArray(nums);
+        maxSum = maxSubArraySum(nums);
         logger.accept(nums, maxSum);
         //
         nums = new int[] {5, 4, -1, 7, 8};
-        maxSum = maxSubArray(nums);
+        maxSum = maxSubArraySum(nums);
         logger.accept(nums, maxSum);
+        //
+        nums = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        maxSum = maxSubArraySumLength(nums);
+        length_logger.accept(nums, maxSum);
+        //
+        nums = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] indices = maxSubArraySumIndices(nums);
+        indices_logger.accept(nums, indices);
+        //
+        nums = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] elements = maxSubArraySumElements(nums);
+        elements_logger.accept(nums, elements);
+        //
+        nums = new int[] {-2, 3, -4};
+        int maxProduct = maxSubArrayProductOptimal(nums);
+        product_logger.accept(nums, maxProduct);
     }
 
 }
