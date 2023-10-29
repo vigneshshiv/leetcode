ea2ba4fb12f3aa5fa13a189b3f28ea02c8529040
vigneshshiv <vichuviky@gmail.com>
2023-10-29 17:38:17 +0530

    solution: 1071 Greatest Common Divisor of Strings - GCD solution

--- code/java/strings/GreatestCommonDivisorOfStrings.java
+++ code/java/strings/GreatestCommonDivisorOfStrings.java
@@ -1,10 +1,10 @@
 package code.java.strings;
 
-import java.util.LinkedList;
 import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
-import java.util.stream.Collectors;
+
+import code.java.utils.MethodsUtility;
 
 /**
  * https://leetcode.com/problems/greatest-common-divisor-of-strings/
@@ -39,25 +39,16 @@ public class GreatestCommonDivisorOfStrings {
     }
 
     /**
-     * Not working for edge cases
+     * Optimal
+     * 
+     * Time complexity: O(m + n), where m is str1, n is str2
+     * Space complexity: O(1)
      */
-    public static String gcdOfStringsTrial(String str1, String str2) {
-        int str1Len = str1.length(), str2Len = str2.length();
-        if (str1.charAt(0) != str2.charAt(0)) {
+    public static String gcdOfStringsOptimal(String str1, String str2) {
+        if (!Objects.equals(str1 + str2, str2 + str1)) {
             return "";
         }
-        LinkedList<Character> divisor = new LinkedList<>();
-        for (int i = 0; i < Math.min(str1Len, str2Len); i++) {
-            if (str1.charAt(i) == str2.charAt(i)) {
-                if (Objects.nonNull(divisor.peekFirst()) && str1.charAt(i) == divisor.peekFirst()) {
-                    break;
-                }
-                divisor.add(str2.charAt(i));
-            } else if (!divisor.isEmpty()) {
-                return "";
-            }
-        }
-        return divisor.stream().map(String::valueOf).collect(Collectors.joining());
+        return str1.substring(0, MethodsUtility.gcd(str1.length(), str2.length()));
     }
 
     public static void main(String[] args) {
@@ -68,25 +59,25 @@ public class GreatestCommonDivisorOfStrings {
         String str1 = "ABCABC", str2 = "ABC";
         String result = gcdOfStrings(str1, str2);
         logger.accept(new String[] {str1, str2}, result);
-        result = gcdOfStringsTrial(str1, str2);
+        result = gcdOfStringsOptimal(str1, str2);
         logger.accept(new String[] {str1, str2}, result);
         //
         str1 = "ABABAB"; str2 = "ABAB";
         result = gcdOfStrings(str1, str2);
         logger.accept(new String[] {str1, str2}, result);
-        result = gcdOfStringsTrial(str1, str2);
+        result = gcdOfStringsOptimal(str1, str2);
         logger.accept(new String[] {str1, str2}, result);
         //
         str1 = "LEET"; str2 = "CODE";
         result = gcdOfStrings(str1, str2);
         logger.accept(new String[] {str1, str2}, result);
-        result = gcdOfStringsTrial(str1, str2);
+        result = gcdOfStringsOptimal(str1, str2);
         logger.accept(new String[] {str1, str2}, result);
         //
         str1 = "TAUXXTAUXXTAUXXTAUXXTAUXX"; str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX";
         result = gcdOfStrings(str1, str2);
         logger.accept(new String[] {str1, str2}, result);
-        result = gcdOfStringsTrial(str1, str2);
+        result = gcdOfStringsOptimal(str1, str2);
         logger.accept(new String[] {str1, str2}, result);
     }
 
