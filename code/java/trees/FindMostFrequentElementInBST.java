c0beb093f14b51b06dcd2f75ee308ca9b0f76f71
vigneshshiv <vichuviky@gmail.com>
2023-11-04 21:09:58 +0530

    solution: 501 - Find Mode in Binary Search Tree

--- code/java/trees/FindMostFrequentElementInBST.java
+++ code/java/trees/FindMostFrequentElementInBST.java
@@ -2,7 +2,12 @@ package code.java.trees;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  * https://leetcode.com/problems/find-mode-in-binary-search-tree/
@@ -42,6 +47,74 @@ public class FindMostFrequentElementInBST {
         findMostFrequentElements(root.right, list);
     }
 
+    private static int[] findModeOptimal(TreeNode root) {
+        Map<Integer, Integer> frequency = new HashMap<>();
+        findModeRecursive(root, frequency);
+        // Leetcode compilation issue for Optional class, so used java.util package directly here
+        java.util.Optional<Integer> max = frequency.values().stream().max(Integer::compare);
+        return max.isPresent() 
+            ? frequency.entrySet().stream()
+                .filter(entry -> entry.getValue() == max.get())
+                .mapToInt(Map.Entry::getKey)
+                .toArray() 
+            : null;
+    }
+
+    private static void findModeRecursive(TreeNode node, Map<Integer, Integer> frequency) {
+        if (node == null) return;
+        // Count the frequency
+        frequency.put(node.data, frequency.getOrDefault(node.data, 0) + 1);
+        findModeRecursive(node.left, frequency);
+        findModeRecursive(node.right, frequency);
+    }
+
+    private static int[] findModeIterative(TreeNode root) {
+        Map<Integer, Integer> frequency = new HashMap<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                int key = stack.peek().data;
+                frequency.put(key, frequency.getOrDefault(key, 0) + 1);
+                root = stack.pop().right;
+            }
+        }
+        // Leetcode compilation issue for Optional class, so used java.util package directly here
+        java.util.Optional<Integer> max = frequency.values().stream().max(Integer::compare);
+        return max.isPresent() 
+            ? frequency.entrySet().stream()
+                .filter(entry -> entry.getValue() == max.get())
+                .mapToInt(Map.Entry::getKey)
+                .toArray()
+            : null;
+    }
+
+    private static int[] findModeBFS(TreeNode root) {
+        Map<Integer, Integer> frequency = new HashMap<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            frequency.put(node.data, frequency.getOrDefault(node.data, 0) + 1);
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        // Leetcode compilation issue for Optional class, so used java.util package directly here
+        java.util.Optional<Integer> max = frequency.values().stream().max(Integer::compare);
+        return max.isPresent() 
+            ? frequency.entrySet().stream()
+                .filter(entry -> entry.getValue() == max.get())
+                .mapToInt(Map.Entry::getKey)
+                .toArray()
+            : null;
+    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.right = new TreeNode(2);
@@ -51,11 +124,37 @@ public class FindMostFrequentElementInBST {
         System.out.println();
         int[] result = findMode(root);
         System.out.println(Arrays.toString(result));
+        result = findModeOptimal(root);
+        System.out.println(Arrays.toString(result));
+        result = findModeIterative(root);
+        System.out.println(Arrays.toString(result));
+        result = findModeBFS(root);
+        System.out.println(Arrays.toString(result));
         //
         root = new TreeNode(1);
         TreeNode.printPreOrderTraversal(root);
         System.out.println();
         result = findMode(root);
+        System.out.println(Arrays.toString(result));
+        result = findModeOptimal(root);
+        System.out.println(Arrays.toString(result));
+        result = findModeIterative(root);
+        System.out.println(Arrays.toString(result));
+        result = findModeBFS(root);
+        System.out.println(Arrays.toString(result));
+        //
+        root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        //
+        TreeNode.printPreOrderTraversal(root);
+        System.out.println();
+        result = findMode(root);
+        System.out.println(Arrays.toString(result));
+        result = findModeOptimal(root);
+        System.out.println(Arrays.toString(result));
+        result = findModeIterative(root);
+        System.out.println(Arrays.toString(result));
+        result = findModeBFS(root);
         System.out.println(Arrays.toString(result));
     }
 
